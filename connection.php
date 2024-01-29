ea7350e08856ddc5f75907a5ce7f669f50cb2157
MithunRR <mithunrrathod7@gmail.com>
2024-01-30 00:12:28 +0530

    db confid changwe

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 
 $dbhost = "monorail.proxy.rlwy.net";
 $dbuseer = "root";
-$dbpass = "2aGBaaah1hGEcCEFCAACcfh42eGD6dEF";
+$dbpass = "bhA52G4ce3Dgaa6b6gf-g4gF5ca5deb5";
 $dbname = "railway";
 
 if (!$con = mysqli_connect($dbhost, $dbuseer, $dbpass, $dbname)){
